e98fad893bcbac2f0f7a76298836cf45f853ab83
Trouvin Paul <paul.trouvin@gmail.com>
2018-01-27 16:28:38 +0100

    16h28

--- ProjetAirChance/src/Tables/AvionPassager.java
+++ ProjetAirChance/src/Tables/AvionPassager.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import BD.DBManager;
-import static Tables.AvionFret.importTable;
 import java.util.ArrayList;
 
 /**
