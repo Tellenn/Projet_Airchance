c1fcd18e093f4f92a64dc69ee03d9f85c35b5d5e
Kyllian Perrin <kyllian.perrin@gmail.com>
2018-01-30 11:03:00 +0100

    Add files via upload

--- Docs/TestTrigger/testCoheDistRayon.sql
+++ Docs/TestTrigger/testCoheDistRayon.sql
@@ -1,2 +1,5 @@
-INSERT INTO Vol VALUES (66, 0, 480, 5835, 150, 3, 50, null, 1, 9);
-INSERT INTO InstanceVol VALUES (1, 66, 20, 0, 0, 0, null, TO_DATE('2017/12/22 10:00:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/01/28 20:55:00', 'yyyy/mm/dd hh24:mi:ss'), 'Arrive');
+-- On creer un vol qui a une distance de 150000
+INSERT INTO Vol VALUES (66, 0, 480, 150000, 0, 0, 0, null, 1, 9);
+-- On essai d'y affecter un avion qui n'aura pas assez de range
+INSERT INTO InstanceVol VALUES (66, 66, 25, 0, 0, 0, null, TO_DATE('2017/12/22 10:00:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/01/28 20:55:00', 'yyyy/mm/dd hh24:mi:ss'), 'Arrive');
+-- La trigger COHEDISTRAYON doit crash
--- Docs/TestTrigger/testCoheReservPlace.sql
+++ Docs/TestTrigger/testCoheReservPlace.sql
@@ -1,4 +1,7 @@
---A retester
+--On souhaite attribuer deux fois la même place, donc on créer deux reservation( 66 et 67)
+-- Et on va leur attribuer la place 2 de l'avion 21 tout les deux
 INSERT INTO ReservationPassager VALUES (66, 16, TO_DATE('2017/12/08 10:08:00', 'yyyy/mm/dd hh24:mi:ss'));
-INSERT INTO ResaVolPlace VALUES (66, 2, 2, 21, 12000);
-INSERT INTO ResaVolPlace VALUES (66, 2, 3, 21, 12000);
+INSERT INTO ResaVolPlace VALUES (66, 4, 2, 21, 12000);
+INSERT INTO ReservationPassager VALUES (67,16, TO_DATE('2017/12/08 10:08:00', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ResaVolPlace VALUES (67, 4, 2, 21, 12000);
+--Déclanchement du trigger coheReservPlace
